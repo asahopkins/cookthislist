b1e334d84248d97c12dea2f4c4ac7d0a713f7cb4
Asa Hopkins <asa.hopkins@me.com>
2012-03-25 12:00:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookthislist::Application.config.secret_token = 'b5cf4e0650f087ca548594f5887ca591df79c38ef0be4ccbdd174a680e381fd95f4fdb06aad45614a0c2c898dc4b8886505c29a63122f6620f3185f5ff4f5449'
